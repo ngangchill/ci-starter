69025befbb83f41223ac06b5f818cf900e8057c0
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2014-12-19 09:22:02 +0200

    Fixed indents

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
 var gulp = require('gulp'),
-	concat = require('gulp-concat');
+    concat = require('gulp-concat');
 
 gulp.task(
-	'default', 
-	[
-		'js',
-		'css'
-	]
+    'default', 
+    [
+        'js',
+        'css'
+    ]
 );
 
 
 gulp.task('js', function() {
 
-	return gulp.src([
-				'bower_components/jquery/dist/jquery.min.js',
-				'bower_components/bootstrap/dist/js/bootstrap.min.js'
-			])
-    	.pipe(concat('scripts.min.js'))
-	    .pipe(gulp.dest('public_html/js'));
+    return gulp.src([
+                'bower_components/jquery/dist/jquery.min.js',
+                'bower_components/bootstrap/dist/js/bootstrap.min.js'
+            ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest('public_html/js'));
 
 });
 
 gulp.task('css', function() {
 
-	return gulp.src([
-				'bower_components/bootstrap/dist/css/bootstrap.min.css'
-			])
-    	.pipe(concat('styles.min.css'))
-	    .pipe(gulp.dest('public_html/css'));
+    return gulp.src([
+                'bower_components/bootstrap/dist/css/bootstrap.min.css'
+            ])
+        .pipe(concat('styles.min.css'))
+        .pipe(gulp.dest('public_html/css'));
 
 });
